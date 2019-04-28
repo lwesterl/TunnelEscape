acda46b215b4d46e43ee6b8ee93e37bf5cf71534
lwesterl <lauri.v.westerholm@gmail.com>
2019-04-29 01:01:58 +0300

    adjust to follow the changes in layouts

--- app/src/main/java/com/westerholmgmail/v/lauri/UI/MenuScreen.java
+++ app/src/main/java/com/westerholmgmail/v/lauri/UI/MenuScreen.java
@@ -5,19 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.westerholmgmail.v.lauri.tunnelescape.GameEngine;
 import com.westerholmgmail.v.lauri.tunnelescape.resources.ResourceManager;
 import com.westerholmgmail.v.lauri.tunnelescape.R;
 
-public class MenuScreen extends AppCompatActivity implements View.OnClickListener {
+public class MenuScreen extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener {
     private Button singlePlayerButton;
     private Button exitButton;
+    private ImageButton boostButton;
+    private ImageButton leftArrowButton;
+    private ImageButton rightArrowButton;
     private ImageView mainMenuImage;
     private GameEngine gameEngine;
     public static int ScreenWidth; /**< tells screen width */
@@ -30,6 +35,10 @@ public class MenuScreen extends AppCompatActivity implements View.OnClickListene
     }
 
 
+    /**
+     * @brief Entry point for TunnelEscape app
+     * @param savedInstanceState possible previous app state
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,20 +50,24 @@ public class MenuScreen extends AppCompatActivity implements View.OnClickListene
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                              WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.main_menu_layout);
-        gameEngine = findViewById(R.id.gameEngine);
         InitScreenSize();
-        CreateUI();
+        CreateMenuUI();
     }
 
+    /**
+     * @brief Handle user button clicks
+     * @param v View which created the click
+     */
     @Override
     public void onClick(View v) {
         switch(v.getId()) {
             case R.id.singlePlayerButton:
                 hideMenu();
-                gameEngine.setGameState(GameState.SinglePlayer);
+                LoadSinglePlayerUI();
                 break;
             case R.id.exitButton:
-                gameEngine.exitGame();
+                //gameEngine.exitGame();
+                exit();
                 break;
             default:
                 System.out.println("Unhandled click");
@@ -62,9 +75,43 @@ public class MenuScreen extends AppCompatActivity implements View.OnClickListene
     }
 
     /**
+     * @brief onTouch implementation, passes button press info to gameEngine
+     * @param v View which created the event
+     * @param event touch event
+     * @return true if this handled the event, otherwise false
+     */
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch(v.getId()) {
+            case R.id.boostButton:
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    gameEngine.boostButtonClicked(true);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    gameEngine.boostButtonClicked(false);
+                }
+                return true;
+            case R.id.leftArrowButton:
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    gameEngine.leftButtonClicked(true);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    gameEngine.leftButtonClicked(false);
+                }
+                return true;
+            case R.id.rightArrowButton:
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    gameEngine.rightButtonClicked(true);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    gameEngine.rightButtonClicked(false);
+                }
+                return true;
+        }
+        return false;
+    }
+
+    /**
      * @brief Create UI for TunnelEscape
      */
-    private void CreateUI() {
+    private void CreateMenuUI() {
         // create Buttons
         singlePlayerButton = findViewById(R.id.singlePlayerButton);
         singlePlayerButton.setOnClickListener(this);
@@ -103,5 +150,22 @@ public class MenuScreen extends AppCompatActivity implements View.OnClickListene
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         MenuScreen.ScreenHeight = displayMetrics.heightPixels;
         MenuScreen.ScreenWidth = displayMetrics.widthPixels;
+    }
+
+    /**
+     * @brief Load UI for single player
+     * @details Changes view to SinglePlayer.
+     * Creates boostButton, leftArrowButton and rightArrowButton
+     */
+    private void LoadSinglePlayerUI() {
+        setContentView(R.layout.single_player_layout);
+        gameEngine = findViewById(R.id.gameEngine);
+        gameEngine.setGameState(GameState.SinglePlayer);
+        boostButton = findViewById(R.id.boostButton);
+        leftArrowButton = findViewById(R.id.leftArrowButton);
+        rightArrowButton = findViewById(R.id.rightArrowButton);
+        boostButton.setOnTouchListener(this);
+        leftArrowButton.setOnTouchListener(this);
+        rightArrowButton.setOnTouchListener(this);
     }
 }
