b1b492a0b6f8b0629aaadb551c9c857a8b6fb540
lwesterl <lauri.v.westerholm@gmail.com>
2019-05-09 00:06:07 +0300

    add LevelItems

--- LevelEditor/include/Editor.hpp
+++ LevelEditor/include/Editor.hpp
@@ -5,10 +5,27 @@
   */
 
 #pragma once
+#include "AssetManager.hpp"
+#include "LevelItem.hpp"
+
 #include <QGraphicsScene>
 #include <QGraphicsSceneMouseEvent>
 #include <QGraphicsRectItem>
 #include <QDebug>
+#include <deque>
+
+
+
+/**
+  *   @enum EditorMode
+  *   @brief Used to detect current editor mode
+  */
+enum class EditorMode {
+  PreviewMode, /**< No insertion or removal allowed */
+  InsertMode, /**< Possible to insert new LevelItems */
+  RemoveMode /**< Possible to remove existing LevelItems */
+};
+
 
 
 /**
@@ -22,6 +39,8 @@ class Editor: public QGraphicsScene {
   public:
     static const int ScreenWidth = 20000; /**< Screen width */
     static const int ScreenHeight = 10000; /**< Screen height */
+    static AssetManager::ImageAssets LevelItemImageAsset; /**< image asset used to create LevelItems */
+    static EditorMode CurrentEditorMode; /**< This should changed via EditorWindow */
 
     /**
       *   @brief Constructor
@@ -34,14 +53,67 @@ class Editor: public QGraphicsScene {
       */
     virtual ~Editor();
 
+    /**
+      *   @brief Add new LevelItem to the level
+      *   @param x mouse center x coordinate
+      *   @param y mouse center y coordinate
+      */
+    void addLevelItem(float x, float y);
+
+    /**
+      *   @brief Try to remove LevelItem from the level
+      *   @param x mouse center x coordinate
+      *   @param y mouse center y coordinate
+      *   @remark This is slow, linear time complexity. However, if this becomes sluggish
+      *   then the level will most likely also play really sluggishly
+      *   @details Tries to remove first a newer object which is pushed to the
+      *   front of the container
+      */
+    void removeLevelItem(float x, float y);
+
+    /**
+      *   @brief Remove current LevelItem
+      *   @details removes the item from scene and deallocates memory
+      */
+    void removeCurrentLevelItem();
+
 
   protected:
+    /**
+      *   @brief Mouse move event implementation
+      *   @param mouseEvent move event
+      */
     void mouseMoveEvent(QGraphicsSceneMouseEvent *mouseEvent) override;
+
+    /**
+      *   @brief Mouse press event implementation
+      *   @param mouseEvent mouse press event
+      */
     void mousePressEvent(QGraphicsSceneMouseEvent *mouseEvent) override;
     void mouseReleaseEvent(QGraphicsSceneMouseEvent *mouseEvent) override;
 
+    /**
+      *   @brief Move currentItem
+      *   @param x new mouse center x coordinate
+      *   @param y new mouse center y coordinate
+      *   @remark does nothing if currentItem is nullptr
+      */
+    void MoveCurrentLevelItem(float x, float y);
+
+    /**
+      *   @brief Insert LevelItem
+      *   @details This is called from mousePressEvent. On the first click this
+      *   creates a new currentItem and on the second click currentItem is
+      *   added to levelItems. This is just basically a wrapper method
+      *   @param x mouse center x coordinate
+      *   @param y mouse center y coordinate
+      */
+    void InsertLevelItem(float x, float y);
+
 
   private:
+    std::deque<LevelItem*> levelItems; // use deque to push items front
+    LevelItem *currentItem = nullptr;
 
 
 };
--- LevelEditor/src/Editor.cpp
+++ LevelEditor/src/Editor.cpp
@@ -6,26 +6,106 @@
 
 #include "../include/Editor.hpp"
 
+// Init image asset for LevelItem creation
+AssetManager::ImageAssets Editor::LevelItemImageAsset = AssetManager::ImageAssets::BlackGround; // this just happens to be the first value
+EditorMode Editor::CurrentEditorMode = EditorMode::InsertMode; // change this to PreviewMode
+
 // Consructor
 Editor::Editor(QObject *parent): QGraphicsScene(parent) {
   setSceneRect(0, 0, Editor::ScreenWidth, Editor::ScreenHeight);
-  addItem(new QGraphicsRectItem(0.f, 0.f, 100.f, 100.f));
 }
 
 // Deconstructor
-Editor::~Editor() {}
+Editor::~Editor() {
+  for (auto it = levelItems.begin(); it != levelItems.end(); it++) {
+    removeItem(*it); // remove from scene
+    delete *it;
+  }
+  levelItems.clear();
+}
 
 // Mouse move event implementation, protected
 void Editor::mouseMoveEvent(QGraphicsSceneMouseEvent *mouseEvent) {
-  qDebug() << mouseEvent->scenePos();
+  float x = mouseEvent->scenePos().x();
+  float y = mouseEvent->scenePos().y();
+  switch (Editor::CurrentEditorMode) {
+    case EditorMode::InsertMode:
+      MoveCurrentLevelItem(x, y);
+      break;
+    case EditorMode::RemoveMode:
+      break;
+    case EditorMode::PreviewMode:
+      break;
+  }
 }
 
 // Mouse press event implementation, protected
 void Editor::mousePressEvent(QGraphicsSceneMouseEvent *mouseEvent) {
-  //qDebug() << mouseEvent->pos();
+  float x = mouseEvent->scenePos().x();
+  float y = mouseEvent->scenePos().y();
+  switch (Editor::CurrentEditorMode) {
+    case EditorMode::InsertMode:
+      InsertLevelItem(x, y);
+      break;
+    case EditorMode::RemoveMode:
+      removeLevelItem(x, y);
+      break;
+    case EditorMode::PreviewMode:
+      break;
+  }
+
 }
 
 // Mouse release event implementation, protected
 void Editor::mouseReleaseEvent(QGraphicsSceneMouseEvent *mouseEvent) {
   //qDebug() << mouseEvent->pos();
+}
+
+// Add LevelItem
+void Editor::addLevelItem(float x, float y) {
+  LevelItem *levelItem = new LevelItem(Editor::LevelItemImageAsset, x, y);
+  addItem(levelItem);
+  levelItems.push_front(levelItem); // this way newer objects get removed first
+}
+
+// Try to remove LevelItems
+void Editor::removeLevelItem(float x, float y) {
+  for (auto it = levelItems.begin(); it != levelItems.end(); it++) {
+    if ((*it)->isInside(x, y)) {
+      // remove item
+      removeItem(*it);
+      delete *it;
+      it = levelItems.erase(it);
+      return;
+    }
+  }
+}
+
+// Remove current LevelItem
+void Editor::removeCurrentLevelItem() {
+  if (currentItem) {
+    removeItem(currentItem);
+    delete currentItem;
+    currentItem = nullptr;
+  }
+}
+
+// Move current LevelItem, protected
+void Editor::MoveCurrentLevelItem(float x, float y) {
+  if (currentItem) {
+    currentItem->setPosition(x, y);
+  }
+}
+
+// Insert a LevelItem, procted
+void Editor::InsertLevelItem(float x, float y) {
+  if (currentItem) {
+    // remove currentItem and create whole new item to levelItems, this is just easier this way
+    removeCurrentLevelItem();
+    addLevelItem(x, y);
+  } else {
+    // create new currentItem and add it to scene
+    currentItem = new LevelItem(Editor::LevelItemImageAsset, x, y);
+    addItem(currentItem);
+  }
 }
